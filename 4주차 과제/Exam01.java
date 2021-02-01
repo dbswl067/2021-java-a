e486668131774a7dee6745442a40d7c639d9874e
dbswl067 <76262688+dbswl067@users.noreply.github.com>
2021-02-01 23:07:31 +0900

    feat: 자바 실습

---
+++ 4주차 과제/Exam01.java
@@ -0,0 +1,11 @@
+public class Exam01 {
+    public static void main(String[] args) {
+        int numberOne = 2100000000;
+        int numberTwo = 2000000000;
+
+        int sum = (numberOne + numberTwo) /2;
+
+        System.out.println("sum = " + sum);
+
+    }
+}
---
+++ 4주차 과제/Exam02.java
@@ -0,0 +1,22 @@
+public class Exam02 {
+
+    // TODO numbers라는 Int형 배열이 있다.
+    //  해당 배열에 들어있는 숫자들은 오직 한 숫자를 제외하고는 모두 두번씩 들어있다.
+    //  오직 한 번만 등장하는 숫자를 찾는 코드를 작성하라.
+    public static void main(String[] args) {
+        Exam02 exam02 = new Exam02();
+
+        int result = exam02.solution(new int[]{5, 4, 3, 2, 1, 2, 3, 4, 5});
+        int result2 = exam02.solution(new int[]{5, 3, 3, 4, 4, 2, 2, 1, 1});
+        System.out.println("result = " + result);
+        System.out.println("result2 = " + result2);
+    }
+
+    public int solution(int[] numbers) {
+        int result = 0;
+        for (int number : numbers) {
+            result ^= number;
+        }
+        return result;
+    }
+}
