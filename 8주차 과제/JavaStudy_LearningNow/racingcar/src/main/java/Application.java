0b679840f3778f00bd0df79a281e8b2699414a4c
dbswl067 <nohyg7482@gmail.com>
2021-04-02 18:02:47 +0900

    add: List

--- 8주차 과제/JavaStudy_LearningNow/racingcar/src/main/java/Application.java
+++ 8주차 과제/JavaStudy_LearningNow/racingcar/src/main/java/Application.java
@@ -1,5 +1,8 @@
+import utils.GameUtils;
+
 public class Application {
     public static void main(String[] args) {
-
+        GameUtils game = new GameUtils();
+        game.run();
     }
 }
--- 8주차 과제/JavaStudy_LearningNow/racingcar/src/main/java/view/InputView.java
+++ 8주차 과제/JavaStudy_LearningNow/racingcar/src/main/java/view/InputView.java
@@ -1,14 +1,16 @@
 package view;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static String[] inputCarName(){
+    public static List<String> inputCarName(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] carName = scanner.nextLine().split(",");
-        return carName;
+        return (List<String>) Arrays.stream(carName);
     }
 
     public static int inputNumber(){
